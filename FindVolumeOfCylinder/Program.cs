94c5ef7acdb7386c648ce45a3902682755367e31
Kimiko Muwanguzi <kimikomu@gmail.com>
2018-01-04 14:46:34 -0500

    init commit

---
+++ FindVolumeOfCylinder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FindVolumeOfCylinder
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double pi = 3.142;
+
+            // radius in centimeters
+            double cylinderRadius = 3.0;
+
+            // height in centimeters
+            double cylinderHeight = 4.0;
+
+            // area = pi * radius squared
+            double area = pi * (cylinderRadius * cylinderRadius);
+
+            // multiply the area by the height
+            double cylinderVolume = cylinderHeight * area;
+
+            Console.WriteLine($"Volume of the cylinder: {cylinderVolume}");
+            Console.Read();
+        }
+    }
+}
